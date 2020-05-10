a2b70057a8a563f697dc767b2b6591e7a5da7766
Samuel Pierre <samuel.pierre37@yahoo.fr>
2020-05-10 12:29:49 +0200

    Trying to prepare the repository for deployment on heroku

--- vocabulary/vocabulary/settings.py
+++ vocabulary/vocabulary/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,3 +136,18 @@ LOGOUT_REDIRECT_URL = '/public/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+#########################################################
+# Mozilla Developer tutorial for deploying a Django App #
+#########################################################
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
