4e7c1eaaf3f4f841bbac4d87ea10571d9330caee
Samuel Pierre <samuel.pierre37@yahoo.fr>
2020-05-10 13:57:49 +0200

    Committing changes

--- vocabulary/vocabulary/settings.py
+++ vocabulary/vocabulary/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 	'crispy_forms',
     'dictionary.apps.DictionaryConfig',
-    'accounts.apps.AccountsConfig',
-]
+    'accounts.apps.AccountsConfig',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -81,9 +80,27 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'samuelp',
+        'PASSWORD': 'PWV5K.8.m#*;d3"qK1',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
+#print(os.path.join(BASE_DIR, 'db.sqlite3'))
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'local_vocabulary',
+        'USER': 'samuelp',
+        'PASSWORD': 'PWV5K.8.m#*;d3"qK1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
