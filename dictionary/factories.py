da3405f3d44f6b46294864500c7132a7b68ab70a
Samuel Pierre <samuel.pierre37@yahoo.fr>
2020-05-11 17:20:43 +0200

    * Putting in place tests to test whether the list of books displayed for a user is correct

--- dictionary/factories.py
+++ dictionary/factories.py
@@ -1,15 +1,30 @@
 import factory
 from django.contrib.auth.models import User
+from .models import Book, Author, Language
+import random
 
-class UserFactoryEmpty(factory.Factory):
+class LanguageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Language
+    name = random.choice(["French","English","German"])
+
+class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    username = factory.Faker('email')
 
-    first_name = 'User'
-    last_name = 'NoAdmin'
-
-class UserFactoryFull(factory.Factory):
+class AuthorFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = User
+        model = Author
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
 
-    # TODO : Create a complete profile
+class BookFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Book
+    author = factory.SubFactory(AuthorFactory)
+    title = factory.Faker('sentence')
+    user = factory.SubFactory(UserFactory)
+    language = factory.SubFactory(LanguageFactory)
--- dictionary/tests.py
+++ dictionary/tests.py
@@ -1,7 +1,8 @@
 from django.test import RequestFactory, TestCase
-from .factories import UserFactoryEmpty
+from .factories import UserFactory, BookFactory
 from django.contrib.auth.models import AnonymousUser
 from .views import *
+from . import models
 
 class TestHomeNoMember(TestCase):
     def test_home_nomember(self):
@@ -17,9 +18,8 @@ class TestBooksList(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
-        self.user_empty = UserFactoryEmpty.create()
 
-    def test_books_list(self):
+    def test_books_list_anonymous(self):
         request = self.factory.get('/public/member/books/list')
         request.user = AnonymousUser()
         response = BooksListView.as_view()(request)
@@ -29,11 +29,36 @@ class TestBooksList(TestCase):
         self.assertIn("/accounts/login",response.url)
 
     def test_books_list_logged_empty(self):
+
+        # User not linked with any book
+        user_empty = UserFactory.create()
+
         request = self.factory.get('/public/member/books/list')
-        request.user = self.user_empty
+        request.user = user_empty
         response = BooksListView.as_view()(request)
         
         # Checking whether the user with no activity does not really see any book
         self.assertContains(response,'No lectures yet')
         self.assertEqual(len(response.context_data['object_list']),0)
         self.assertEqual(response.status_code, 200)
+
+    def test_books_list_logged_not_empty(self):
+
+        # User linked with books
+        user_notempty_1 = UserFactory.create()
+        user_notempty_2 = UserFactory.create()
+        nb_books_1 = 8
+        nb_books_2 = 4
+        for i in range(nb_books_1):
+            BookFactory.create(user=user_notempty_1)
+        for i in range(nb_books_2):
+            BookFactory.create(user=user_notempty_2)
+
+        request = self.factory.get('/public/member/books/list')
+        request.user = user_notempty_1
+        response = BooksListView.as_view()(request)
+
+        # Checking whether the user is displayed with all his books
+        self.assertEqual(len(response.context_data['object_list']),nb_books_1)
+        self.assertEqual(response.status_code, 200)
+
