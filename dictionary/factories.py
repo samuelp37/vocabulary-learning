84e4d39f20bbf1d11e475b5483cf4a6a10ce455f
Samuel Pierre <samuel.pierre37@yahoo.fr>
2020-05-13 18:55:36 +0200

    View of the list of translations now tested (including pagination)

--- dictionary/factories.py
+++ dictionary/factories.py
@@ -1,12 +1,24 @@
 import factory
 from django.contrib.auth.models import User
-from .models import Book, Author, Language
+from . import models
 import random
+from faker.providers import BaseProvider
+from faker import Faker
+
+fake_gender = Faker()
+class GenderProvider(BaseProvider):
+    def gender(self):
+        correspondences = {"M":"Masculine","F":"Feminine","N":"Neutral","-":"-"}
+        shorts = [short for short in correspondences]
+        chosen = random.choice(shorts)
+        long_gender = correspondences[chosen]
+        return chosen,long_gender
+fake_gender.add_provider(GenderProvider)
 
 class LanguageFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = Language
-    name = random.choice(["French","English","German"])
+        model = models.Language
+    name = random.choices(["French","English","German"])
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -17,14 +29,63 @@ class UserFactory(factory.django.DjangoModelFactory):
 
 class AuthorFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = Author
+        model = models.Author
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
 
 class BookFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = Book
+        model = models.Book
     author = factory.SubFactory(AuthorFactory)
     title = factory.Faker('sentence')
     user = factory.SubFactory(UserFactory)
     language = factory.SubFactory(LanguageFactory)
+
+class GenderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Gender
+    short, gender = fake_gender.gender()
+
+class WordFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Word
+    word = factory.Faker('word')
+    plural = factory.Faker('word')
+    gender = factory.SubFactory(GenderFactory)
+    language = factory.SubFactory(LanguageFactory)
+
+class AdjectiveFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Adjective
+    word = factory.Faker('word')
+    plural = factory.Faker('word')
+    language = factory.SubFactory(LanguageFactory)
+
+class VerbFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Verb
+    word = factory.Faker('word')
+    language = factory.SubFactory(LanguageFactory)
+
+class ExpressionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Expression
+    expression = factory.Faker('sentence')
+    language = factory.SubFactory(LanguageFactory)
+
+class TranslationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Translation
+    original_word = factory.SubFactory(WordFactory)
+    translated_word = factory.SubFactory(WordFactory)
+    original_adj = factory.SubFactory(AdjectiveFactory)
+    translated_adj = factory.SubFactory(AdjectiveFactory)
+    original_verb = factory.SubFactory(VerbFactory)
+    translated_verb = factory.SubFactory(VerbFactory)
+    original_exp = factory.SubFactory(ExpressionFactory)
+    translated_exp = factory.SubFactory(ExpressionFactory)
+    date_added = factory.Faker('date')
+    context_sentence = ""
+    translation_context_sentence = ""
+    slug = factory.Faker('slug')
+    user = factory.SubFactory(UserFactory)
--- dictionary/tests.py
+++ dictionary/tests.py
@@ -1,8 +1,9 @@
 from django.test import RequestFactory, TestCase
-from .factories import UserFactory, BookFactory
+from . import factories
 from django.contrib.auth.models import AnonymousUser
 from .views import *
 from . import models
+from django.urls import reverse
 
 class TestHomeNoMember(TestCase):
     def test_home_nomember(self):
@@ -18,11 +19,11 @@ class TestBooksList(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
+        self.request = self.factory.get(reverse('list_book'))
 
     def test_books_list_anonymous(self):
-        request = self.factory.get('/public/member/books/list')
-        request.user = AnonymousUser()
-        response = BooksListView.as_view()(request)
+        self.request.user = AnonymousUser()
+        response = BooksListView.as_view()(self.request)
 
         # Checking whether the user is redirected to the login page
         self.assertEqual(response.status_code, 302)
@@ -31,11 +32,10 @@ class TestBooksList(TestCase):
     def test_books_list_logged_empty(self):
 
         # User not linked with any book
-        user_empty = UserFactory.create()
+        user_empty = factories.UserFactory.create()
 
-        request = self.factory.get('/public/member/books/list')
-        request.user = user_empty
-        response = BooksListView.as_view()(request)
+        self.request.user = user_empty
+        response = BooksListView.as_view()(self.request)
         
         # Checking whether the user with no activity does not really see any book
         self.assertContains(response,'No lectures yet')
@@ -45,20 +45,104 @@ class TestBooksList(TestCase):
     def test_books_list_logged_not_empty(self):
 
         # User linked with books
-        user_notempty_1 = UserFactory.create()
-        user_notempty_2 = UserFactory.create()
-        nb_books_1 = 8
-        nb_books_2 = 4
-        for i in range(nb_books_1):
-            BookFactory.create(user=user_notempty_1)
-        for i in range(nb_books_2):
-            BookFactory.create(user=user_notempty_2)
+        user_notempty_1 = factories.UserFactory.create()
+        user_notempty_2 = factories.UserFactory.create()
+        nb_books_1 = random.randint(1,9)
+        nb_books_2 = random.randint(1,9)
+        for _ in range(nb_books_1):
+            factories.BookFactory.create(user=user_notempty_1)
+        for _ in range(nb_books_2):
+            factories.BookFactory.create(user=user_notempty_2)
 
-        request = self.factory.get('/public/member/books/list')
-        request.user = user_notempty_1
-        response = BooksListView.as_view()(request)
+        self.request.user = user_notempty_1
+        response = BooksListView.as_view()(self.request)
 
         # Checking whether the user is displayed with all his books
         self.assertEqual(len(response.context_data['object_list']),nb_books_1)
         self.assertEqual(response.status_code, 200)
 
+class TranslationsList(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.request = self.factory.get(reverse('list_translations'))
+        if hasattr(TranslationListView,'paginate_by'):
+            self.paginate_by = TranslationListView.paginate_by
+        else:
+            self.paginate_by = False
+
+    def test_translations_list_anonymous(self):
+        self.request.user = AnonymousUser()
+        response = TranslationListView.as_view()(self.request)
+
+        # Checking whether the user is redirected to the login page
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/accounts/login",response.url)
+
+    def test_translations_list_logged_empty(self):
+
+        # User not linked with any book
+        user_empty = factories.UserFactory.create()
+
+        self.request.user = user_empty
+        response = TranslationListView.as_view()(self.request)
+        
+        # Checking whether the user with no activity does not really see any translation
+        self.assertEqual(len(response.context_data['object_list']),0)
+        self.assertEqual(response.status_code, 200)
+
+    def test_translations_list_logged_not_empty(self):
+
+        # User linked with translations
+        user_notempty_1 = factories.UserFactory.create()
+        user_notempty_2 = factories.UserFactory.create()
+        if self.paginate_by:
+            nb_translations_1 = random.randint(1,self.paginate_by)
+        else:
+            nb_translations_1 = random.randint(1,100)
+        nb_translations_2 = random.randint(1,100)
+        for _ in range(nb_translations_1):
+            factories.TranslationFactory.create(user=user_notempty_1)
+        for _ in range(nb_translations_2):
+            factories.TranslationFactory.create(user=user_notempty_2)
+
+        self.request.user = user_notempty_1
+        response = TranslationListView.as_view()(self.request)
+
+        # Checking whether the user is displayed with all his translations
+        self.assertEqual(len(response.context_data['object_list']),nb_translations_1)
+        self.assertEqual(response.status_code, 200)
+
+    def test_translations_with_pagination(self):
+        if self.paginate_by:
+            # User linked with translations
+            user_notempty_1 = factories.UserFactory.create()
+            user_notempty_2 = factories.UserFactory.create()
+            nb_translations_1 = random.randint(self.paginate_by+1,100)
+            nb_translations_2 = random.randint(1,100)
+            for _ in range(nb_translations_1):
+                factories.TranslationFactory.create(user=user_notempty_1)
+            for _ in range(nb_translations_2):
+                factories.TranslationFactory.create(user=user_notempty_2)
+
+            for i in range(1,int(nb_translations_1/self.paginate_by)+1):
+                self.request = self.factory.get(reverse('list_translations'))
+                self.request.GET = self.request.GET.copy()
+                self.request.GET['page'] = i
+                self.request.user = user_notempty_1
+                response = TranslationListView.as_view()(self.request)
+                self.assertEqual(response.status_code, 200)
+                self.assertTrue(len(response.context_data['object_list']) == self.paginate_by)
+            
+            remaining = nb_translations_1 % self.paginate_by
+            if remaining!=0:
+                self.request = self.factory.get(reverse('list_translations'))
+                self.request.user = user_notempty_1
+                self.request.GET = self.request.GET.copy()
+                self.request.GET['page']=int(nb_translations_1/self.paginate_by)+1
+                response = TranslationListView.as_view()(self.request)
+                self.assertEqual(response.status_code, 200)
+                self.assertTrue(len(response.context_data['object_list']) == remaining)
+
+        else:
+            pass
